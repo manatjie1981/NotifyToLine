a102612eb02d2af169ad83af4a7e2fb8364d0b5e
Manat Jieranaiwongkul-APNS (Thailand) <ManatJ-APNS@azay.co.th>
2021-03-05 17:48:01 +0700

    initial

---
+++ src/main/java/com/notify/linenotify/LinenotifyApplication.java
@@ -0,0 +1,13 @@
+package com.notify.linenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinenotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinenotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notify/linenotify/LinenotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notify.linenotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinenotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
